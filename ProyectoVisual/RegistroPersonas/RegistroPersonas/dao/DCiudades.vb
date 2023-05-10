85bf582376584099df8e230511d05b79404c7435
MrGoose750 <elsotelito777@gmail.com>
2023-05-10 14:56:40 -0600

    Practica agregada

---
+++ ProyectoVisual/RegistroPersonas/RegistroPersonas/dao/DCiudades.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.SqlClient
+
+Public Class DCiudades
+
+    Dim strConn As String = My.Settings.StrConnection.ToString()
+
+    Public Function MostrarRegistros() As DataSet
+        Dim ds As New DataSet
+        Try
+            Dim conn As New SqlConnection(strConn)
+            Dim tSql As String = "Select id as N'CODIGO', nombre as N'CIUDAD' from ciudad"
+            Dim da As New SqlDataAdapter(tSql, conn)
+            da.Fill(ds)
+        Catch ex As Exception
+            MsgBox("Ocurrio un error al obtener los registros" & ex.Message, MsgBoxStyle.Critical, "Error")
+        End Try
+
+        Return ds
+    End Function
+End Class
---
+++ ProyectoVisual/RegistroPersonas/RegistroPersonas/formularios/FrmCiudad.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmCiudad
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.BtnNuevo = New System.Windows.Forms.ToolStripButton()
+        Me.BtnGuardar = New System.Windows.Forms.ToolStripButton()
+        Me.BtnEditar = New System.Windows.Forms.ToolStripButton()
+        Me.BtnEliminar = New System.Windows.Forms.ToolStripButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ChkActivo = New System.Windows.Forms.CheckBox()
+        Me.TxtNombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtId = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GbRegistro = New System.Windows.Forms.GroupBox()
+        Me.DgvRegistros = New System.Windows.Forms.DataGridView()
+        Me.ToolStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GbRegistro.SuspendLayout()
+        CType(Me.DgvRegistros, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BtnNuevo, Me.BtnGuardar, Me.BtnEditar, Me.BtnEliminar})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(699, 27)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'BtnNuevo
+        '
+        Me.BtnNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BtnNuevo.Image = Global.RegistroPersonas.My.Resources.Resources.cleanButton
+        Me.BtnNuevo.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnNuevo.Name = "BtnNuevo"
+        Me.BtnNuevo.Size = New System.Drawing.Size(29, 24)
+        Me.BtnNuevo.Text = "Nuevo"
+        '
+        'BtnGuardar
+        '
+        Me.BtnGuardar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BtnGuardar.Image = Global.RegistroPersonas.My.Resources.Resources.addButton24px
+        Me.BtnGuardar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnGuardar.Name = "BtnGuardar"
+        Me.BtnGuardar.Size = New System.Drawing.Size(29, 24)
+        Me.BtnGuardar.Text = "Guardar"
+        '
+        'BtnEditar
+        '
+        Me.BtnEditar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BtnEditar.Image = Global.RegistroPersonas.My.Resources.Resources.editButton
+        Me.BtnEditar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnEditar.Name = "BtnEditar"
+        Me.BtnEditar.Size = New System.Drawing.Size(29, 24)
+        Me.BtnEditar.Text = "Editar"
+        '
+        'BtnEliminar
+        '
+        Me.BtnEliminar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BtnEliminar.Image = Global.RegistroPersonas.My.Resources.Resources.deleteButton
+        Me.BtnEliminar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnEliminar.Name = "BtnEliminar"
+        Me.BtnEliminar.Size = New System.Drawing.Size(29, 24)
+        Me.BtnEliminar.Text = "Eliminar"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ChkActivo)
+        Me.GroupBox1.Controls.Add(Me.TxtNombre)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TxtId)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 30)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(660, 173)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'ChkActivo
+        '
+        Me.ChkActivo.AutoSize = True
+        Me.ChkActivo.Location = New System.Drawing.Point(441, 32)
+        Me.ChkActivo.Name = "ChkActivo"
+        Me.ChkActivo.Size = New System.Drawing.Size(77, 24)
+        Me.ChkActivo.TabIndex = 2
+        Me.ChkActivo.Text = "Activo"
+        Me.ChkActivo.UseVisualStyleBackColor = True
+        '
+        'TxtNombre
+        '
+        Me.TxtNombre.Location = New System.Drawing.Point(132, 72)
+        Me.TxtNombre.Name = "TxtNombre"
+        Me.TxtNombre.Size = New System.Drawing.Size(486, 27)
+        Me.TxtNombre.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(48, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nombre:"
+        '
+        'TxtId
+        '
+        Me.TxtId.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtId.Location = New System.Drawing.Point(132, 29)
+        Me.TxtId.Name = "TxtId"
+        Me.TxtId.ReadOnly = True
+        Me.TxtId.Size = New System.Drawing.Size(271, 27)
+        Me.TxtId.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(16, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Identificador:"
+        '
+        'GbRegistro
+        '
+        Me.GbRegistro.Controls.Add(Me.DgvRegistros)
+        Me.GbRegistro.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GbRegistro.Location = New System.Drawing.Point(12, 225)
+        Me.GbRegistro.Name = "GbRegistro"
+        Me.GbRegistro.Size = New System.Drawing.Size(663, 272)
+        Me.GbRegistro.TabIndex = 2
+        Me.GbRegistro.TabStop = False
+        Me.GbRegistro.Text = "Registros Guardados: 0"
+        '
+        'DgvRegistros
+        '
+        Me.DgvRegistros.AllowUserToAddRows = False
+        Me.DgvRegistros.AllowUserToDeleteRows = False
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.DgvRegistros.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.DgvRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgvRegistros.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DgvRegistros.Location = New System.Drawing.Point(3, 23)
+        Me.DgvRegistros.Name = "DgvRegistros"
+        Me.DgvRegistros.ReadOnly = True
+        Me.DgvRegistros.RowHeadersWidth = 51
+        Me.DgvRegistros.RowTemplate.Height = 24
+        Me.DgvRegistros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DgvRegistros.Size = New System.Drawing.Size(657, 246)
+        Me.DgvRegistros.TabIndex = 0
+        '
+        'FrmCiudad
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(699, 542)
+        Me.Controls.Add(Me.GbRegistro)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "FrmCiudad"
+        Me.Text = "Gestion de Ciudades"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GbRegistro.ResumeLayout(False)
+        CType(Me.DgvRegistros, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents BtnNuevo As ToolStripButton
+    Friend WithEvents BtnGuardar As ToolStripButton
+    Friend WithEvents BtnEditar As ToolStripButton
+    Friend WithEvents BtnEliminar As ToolStripButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtId As TextBox
+    Friend WithEvents ChkActivo As CheckBox
+    Friend WithEvents TxtNombre As TextBox
+    Friend WithEvents GbRegistro As GroupBox
+    Friend WithEvents DgvRegistros As DataGridView
+End Class
